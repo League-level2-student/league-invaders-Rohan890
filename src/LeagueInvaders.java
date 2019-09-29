0d4e4ec306d8aa5647ef66c2b1d192770f2b810e
League Student <league@iMac-7.attlocal.net>
2019-09-29 15:51:40 -0700

    invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,10 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+public static void main(String[] args) {
+	JFrame frame = new JFrame();
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.pack();
+}
 }
